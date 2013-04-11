96b0f7ece376f7d860633d94f30dc1de6778c5c7
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-04-11 19:40:02 +0100

    Updated env to removed capybara/cucumber require and added relevant setup into env and hooks

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'bundler'
 require 'capybara'
-require 'capybara/cucumber'
 require 'capybara/rspec'
 require 'pry'
-
 require './lib/application_world'
 
+World(Capybara::DSL, Capybara::RSpecMatchers)
+
+Capybara.register_driver(:chrome)   { |app| Capybara::Selenium::Driver.new(app, :browser => :chrome) }
+Capybara.register_driver(:firefox)  { |app| Capybara::Selenium::Driver.new(app, :browser => :firefox) }
+
 World do
-	ApplicationWorld.new
+  ApplicationWorld.new
 end
